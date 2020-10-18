1d83165e469b69d618b192d09e85ceb2489dc013
Steamed-Punk <54961735+Steamed-Punk@users.noreply.github.com>
2020-10-18 21:34:08 +0200

    Add files via upload
    added lighting to streetlight

--- streetlight.lua
+++ streetlight.lua
@@ -7,69 +7,44 @@
 
 -- NODES
 
--- well
+-- STREETLIGHT
 
 minetest.register_node("mt_buildings:streetlight", {
-  description = "Street Light",
-  drawtype = "mesh",
-  mesh = "streetlight.obj",
-  use_texture_alpha = true,
-  tiles = {"Lamp.png"},
-  buildable_to = false,
-  paramtype = "light",
-  paramtype2 = "facedir",
-  groups = {choppy=2, oddly_breakable_by_hand=2, flammable=3},
-  on_place = minetest.rotate_node,
+	description = "Street Light",
+	drawtype = "mesh",
+	mesh = "streetlight.obj",
+	tiles = {"Lamp.png"},
+    use_texture_alpha = true,
+	paramtype = "light",
+	paramtype2 = "facedir",
+	sunlight_propagates = true,
+	walkable = false,
+	light_source = 14,
+	groups = {choppy=2, oddly_breakable_by_hand=2, flammable=3},
+	on_place = minetest.rotate_node,
 
-	selection_box = {
-		type = "fixed",
-		fixed = {
-				{-0.4, 1.5, -0.4, 0.4, 2.5, 0.4},
+		selection_box = {
+			type = "fixed",
+			fixed = {
+					{-0.1, 1.7, 0.8, 0.2, 2, 1.1},
+			},
 		},
-	},
-	collision_box = {
-		type = "fixed",
-		fixed = {
-				{-0.4, -0.5, -0.4, 0.4, 2.5, 0.4},
+		collision_box = {
+			type = "fixed",
+			fixed = {
+					{-0.4, -0.5, -0.4, 0.4, 2.5, 0.4},
+			},
 		},
-	},
+	})
+
+
+-- STREETLIGHT_CRAFT
+
+minetest.register_craft({
+		output = "mt_buildings:streetlight",
+		recipe = {
+				{'group:wood', 'group:wood',  'default:glass'},
+				{'group:wood', '',  'default:torch'},
+				{'group:wood', '',  ''},
+		}
 })
-
-
-  -- WELL_CRAFT
-
-  minetest.register_craft({
-      output = "mt_buildings:hut01",
-      recipe = {
-          {'', 'group:tree', ''},
-          {'','group:tree', ''},
-          {'', 'group:tree', ''},
-      }
-  })
-
-  minetest.register_craft({
-      output = "mt_buildings:hut02",
-      recipe = {
-          {'', '', ''},
-          {'group:tree','group:tree', 'group:tree'},
-          {'', '', ''},
-      }
-  })
-
-  minetest.register_craft({
-      output = "mt_buildings:hut03",
-      recipe = {
-          {'', '', 'group:tree'},
-          {'','group:tree', ''},
-          {'group:tree', '', ''},
-      }
-  })
-
-  minetest.register_craft({
-      output = "mt_buildings:hut04",
-      recipe = {
-          {'group:tree', '', ''},
-          {'','group:tree', ''},
-          {'', '', 'group:tree'},
-      }
-  })
